b0a515e74c00962ba8ffe1738852766d9d64d85d
jmirancid <jmirancid@gmail.com>
2015-04-08 01:44:46 -0300

    Agregado paginas estaticas para bluetooth, custom sounds y t20.

---
+++ Topppro.WebSite/Areas/Humanist/Controllers/PublicController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Topppro.WebSite.Areas.Humanist.Controllers
+{
+    public class PublicController : Controller
+    {
+        public ActionResult Bluetooth()
+        {
+            return View();
+        }
+
+        public ActionResult CustomSound()
+        {
+            return View();
+        }
+    }
+}
