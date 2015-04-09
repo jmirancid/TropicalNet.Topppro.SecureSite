968c2917a699cd3c946e70b86a7ba9e5607c1077
jmirancid <jmirancid@gmail.com>
2015-04-09 02:10:33 -0300

    Cambios en Serie I.

--- Topppro.Entities/Model.cs
+++ Topppro.Entities/Model.cs
@@ -25,7 +25,16 @@ namespace Topppro.Entities
     {
         NA = 0,
         Active = 1,
-        Passive = 2
+        Passive = 2,
+        Digital = 3,
+        USB = 4,
+        Digital_FX = 5,
+        _4Bus_Mixer = 6,
+        _10_Inputs = 7,
+        _14_Inputs = 8,
+        _6_Inputs = 9,
+        Coaxial = 10,
+        Combo = 11
     }
 
     public class Model_Metadata
