9d43af42630317b94f28712bac036eada5a37689
jmirancid <jmirancid@gmail.com>
2016-02-21 16:14:32 -0300

    Agregado test para crear usuarios.

---
+++ Topppro.Test/Providers/MembershipProviderTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Security;
+
+namespace Topppro.Test.Providers
+{
+    [TestClass]
+    public class MembershipProviderTest
+    {
+        [TestMethod]
+        public void CreateUserTest()
+        {
+            var adminMember =
+                Membership.CreateUser("topppro_admin", "topppro01*");
+
+            var customerMember =
+                Membership.CreateUser("CLIENT SERVICES", "TOPP2015!");
+
+            Assert.IsNotNull(adminMember);
+            Assert.IsNotNull(customerMember);
+        }
+    }
+}
