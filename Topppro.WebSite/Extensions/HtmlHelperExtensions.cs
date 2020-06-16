5a4c2f6c8fd422b382ffe22925c4fb990f2c8303
Juan <Juan@LENOVO-PC>
2020-06-15 22:47:36 -0300

    Added news page

---
+++ Topppro.WebSite/Extensions/HtmlHelperExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+using Topppro.WebSite.Models;
+
+namespace Topppro.WebSite.Extensions
+{
+    public static class HtmlHelperExtensions
+    {
+        public static MvcHtmlString Article(
+            this HtmlHelper htmlHelper, string image, string title, string content, string info = null, string date = null)
+        {
+            var model = 
+                new Article() { Image = image, Title = title, Content = content, Info = info, Date = date };
+
+            return htmlHelper.Partial("_Article", model);
+        }
+    }
+}
