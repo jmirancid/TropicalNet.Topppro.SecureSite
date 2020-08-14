bdcb79ab13ccec5d7b48495ca86cf5497321205a
Juan <Juan@LENOVO-PC>
2020-08-13 23:27:32 -0300

    fix on menu style added search on distributor page

--- Topppro.WebSite/App_Start/BundleConfig.cs
+++ Topppro.WebSite/App_Start/BundleConfig.cs
@@ -14,6 +14,9 @@ namespace Topppro.WebSite
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/jqueryplug").Include(
+                        "~/Scripts/jquery.plugin*"));
+
             // Utilice la versión de desarrollo de Modernizr para desarrollar y obtener información. De este modo, estará
             // para la producción, use la herramienta de compilación disponible en https://modernizr.com para seleccionar solo las pruebas que necesite.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
