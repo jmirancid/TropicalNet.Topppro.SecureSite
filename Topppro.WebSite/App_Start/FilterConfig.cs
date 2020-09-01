3dd3e7170fb328ad1ec06e8cd3c500ac39b071e6
Juan <Juan@LENOVO-PC>
2020-08-31 23:18:18 -0300

    added esp switching lang filter action

--- Topppro.WebSite/App_Start/FilterConfig.cs
+++ Topppro.WebSite/App_Start/FilterConfig.cs
@@ -8,6 +8,7 @@ namespace Topppro.WebSite
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new HandleCultureAttribute());
         }
     }
 }
---
+++ Topppro.WebSite/Extensions/UrlHelperExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Topppro.WebSite.Extensions
+{
+    public static class UrlHelperExtensions
+    {
+        public static string SwitchCulture(
+            this UrlHelper urlHelper)
+        {
+            var url = 
+                urlHelper.RequestContext.HttpContext.Request.Url.ToString();
+
+            var culture =
+                Context.Current.Culture.TwoLetterISOLanguageName;
+
+            if (url.Contains("/en") == false && url.Contains("/es") == false)
+                url += "/en";
+
+            if (culture == "en")
+                return url.Replace("/" + culture, "/es");
+
+            return url.Replace("/" + culture, "/en");
+        }
+    }
+}
