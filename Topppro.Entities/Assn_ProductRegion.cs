4639fe0986e757dfaa4c7243fecd91cc3a96850c
jmirancid <jmirancid@gmail.com>
2024-07-31 20:40:31 -0300

    added entities

---
+++ Topppro.Entities/Assn_ProductRegion.cs
@@ -0,0 +1,16 @@
+﻿using xFNet.Common.Extensions;
+using xFNet.Entities;
+
+
+namespace Topppro.Entities
+{
+    public partial class Assn_ProductRegion : Entity
+    {
+        public override object Id
+        {
+            get => this.AssnProductRegionId;
+
+            set => this.AssnProductRegionId = value.To<int>();
+        }
+    }
+}
---
+++ Topppro.Entities/Region.cs
@@ -0,0 +1,15 @@
+﻿using xFNet.Common.Extensions;
+using xFNet.Entities;
+
+
+namespace Topppro.Entities
+{
+    public partial class Region : Entity
+    {
+        public override object Id
+        {
+            get => this.RegionId;
+            set => this.RegionId = value.To<int>();
+        }
+    }
+}
