9c27b3d97c226ca866789885f41e69289e580152
jmirancid <jmirancid@gmail.com>
2024-07-31 21:21:04 -0300

    added new repositories

---
+++ Topppro.Repositories/Definitions/AssnProductRegionRepository.cs
@@ -0,0 +1,17 @@
+﻿using Topppro.Interfaces.Repositories;
+
+
+namespace Topppro.Repositories.Definitions
+{
+    public class AssnProductRegionRepository :
+        Repository<Topppro.Entities.Assn_ProductRegion>, IAssnProductRegionRepository
+    {
+        public AssnProductRegionRepository() { }
+
+        public AssnProductRegionRepository(ToppproEntities context) :
+            base(context)
+        {
+
+        }
+    }
+}
