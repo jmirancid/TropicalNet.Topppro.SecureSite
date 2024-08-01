928a9ed21b3c3b1d069938d9d859f7f8edab5337
jmirancid <jmirancid@gmail.com>
2024-08-01 00:20:01 -0300

    added business

---
+++ Topppro.Business/Definitions/AssnProductRegionBusiness.cs
@@ -0,0 +1,19 @@
+﻿using Topppro.Interfaces.Business;
+using Topppro.Interfaces.Repositories;
+using xFNet.Business;
+
+
+namespace Topppro.Business.Definitions
+{
+    public class AssnProductRegionBusiness :
+         Business<Topppro.Entities.Assn_ProductRegion, IAssnProductRegionRepository>, IAssnProductRegionBusiness
+    {
+        public AssnProductRegionBusiness() { }
+
+        public AssnProductRegionBusiness(IAssnProductRegionRepository repository) :
+            base(repository)
+        {
+
+        }
+    }
+}
---
+++ Topppro.Business/Definitions/RegionBusiness.cs
@@ -0,0 +1,19 @@
+﻿using Topppro.Interfaces.Business;
+using Topppro.Interfaces.Repositories;
+using xFNet.Business;
+
+
+namespace Topppro.Business.Definitions
+{
+    public class RegionBusiness :
+        Business<Topppro.Entities.Region, IRegionRepository>, IRegionBusiness
+    {
+        public RegionBusiness() { }
+
+        public RegionBusiness(IRegionRepository repository) :
+            base(repository)
+        {
+
+        }
+    }
+}
