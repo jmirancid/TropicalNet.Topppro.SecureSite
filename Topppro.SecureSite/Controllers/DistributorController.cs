d17ee106f95b36487a8b4118ec9560699fa3dd6f
jmirancid <jmirancid@gmail.com>
2024-08-26 22:35:40 -0300

    fix all crud entities

--- Topppro.SecureSite/Controllers/DistributorController.cs
+++ Topppro.SecureSite/Controllers/DistributorController.cs
@@ -25,62 +25,65 @@ namespace Topppro.SecureSite.Controllers
 
         public override ActionResult Index()
         {
-            return View();
+            var dist =
+                base.Business.All().OrderBy(s => s.Name);
+
+            return View(dist);
         }
 
-        public override System.Web.Mvc.JsonResult Index(string sEcho, string sSearch, int iSortCol_0, string sSortDir_0, int iDisplayStart, int iDisplayLength)
-        {
-            int count;
-            IEnumerable<Topppro.Entities.Distributor> filtered;
-            System.Linq.Expressions.Expression<Func<Topppro.Entities.Distributor, bool>> predicate;
+        //public override System.Web.Mvc.JsonResult Index(string sEcho, string sSearch, int iSortCol_0, string sSortDir_0, int iDisplayStart, int iDisplayLength)
+        //{
+        //    int count;
+        //    IEnumerable<Topppro.Entities.Distributor> filtered;
+        //    System.Linq.Expressions.Expression<Func<Topppro.Entities.Distributor, bool>> predicate;
 
-            if (!string.IsNullOrEmpty(sSearch))
-            {
-                predicate =
-                    x => x.Country.Name.ToLower().Contains(sSearch.ToLower())
-                         || x.Culture.Name.ToLower().Contains(sSearch.ToLower())
-                         || x.Name.ToLower().Contains(sSearch.ToLower());
+        //    if (!string.IsNullOrEmpty(sSearch))
+        //    {
+        //        predicate =
+        //            x => x.Country.Name.ToLower().Contains(sSearch.ToLower())
+        //                 || x.Culture.Name.ToLower().Contains(sSearch.ToLower())
+        //                 || x.Name.ToLower().Contains(sSearch.ToLower());
 
-                count =
-                    base.Business.CountBy(predicate);
+        //        count =
+        //            base.Business.CountBy(predicate);
 
-                filtered =
-                    base.Business.FilterBy(iDisplayStart, iDisplayLength, predicate);
-            }
-            else
-            {
-                count =
-                    base.Business.Count();
+        //        filtered =
+        //            base.Business.FilterBy(iDisplayStart, iDisplayLength, predicate);
+        //    }
+        //    else
+        //    {
+        //        count =
+        //            base.Business.Count();
 
-                filtered =
-                    base.Business.Filter(iDisplayStart, iDisplayLength);
-            }
+        //        filtered =
+        //            base.Business.Filter(iDisplayStart, iDisplayLength);
+        //    }
 
-            if (sSortDir_0 == "asc")
-            {
-                filtered = filtered.OrderBy(x =>
-                                  (iSortCol_0 == 0) ? x.Country.Name
-                                : (iSortCol_0 == 1) ? x.Culture.Name : x.Name);
-            }
-            else
-            {
-                filtered = filtered.OrderByDescending(x =>
-                                  (iSortCol_0 == 0) ? x.Country.Name
-                                : (iSortCol_0 == 1) ? x.Culture.Name : x.Name);
-            }
+        //    if (sSortDir_0 == "asc")
+        //    {
+        //        filtered = filtered.OrderBy(x =>
+        //                          (iSortCol_0 == 0) ? x.Country.Name
+        //                        : (iSortCol_0 == 1) ? x.Culture.Name : x.Name);
+        //    }
+        //    else
+        //    {
+        //        filtered = filtered.OrderByDescending(x =>
+        //                          (iSortCol_0 == 0) ? x.Country.Name
+        //                        : (iSortCol_0 == 1) ? x.Culture.Name : x.Name);
+        //    }
 
-            var data = filtered
-                .Select(x => new { Id = x.Id, Country = x.Country.Name, Culture = x.Culture.Name, Name = x.Name, Priority = x.Priority, Enabled = x.Enabled });
+        //    var data = filtered
+        //        .Select(x => new { Id = x.Id, Country = x.Country.Name, Culture = x.Culture.Name, Name = x.Name, Priority = x.Priority, Enabled = x.Enabled });
 
-            return
-                Json(new
-                {
-                    sEcho = sEcho,
-                    iTotalRecords = base.Business.Count(),
-                    iTotalDisplayRecords = count,
-                    aaData = data
-                }, JsonRequestBehavior.AllowGet);
-        }
+        //    return
+        //        Json(new
+        //        {
+        //            sEcho = sEcho,
+        //            iTotalRecords = base.Business.Count(),
+        //            iTotalDisplayRecords = count,
+        //            aaData = data
+        //        }, JsonRequestBehavior.AllowGet);
+        //}
 
         [ValidateInput(false)]
         public override ActionResult Create(Entities.Distributor entity)
@@ -94,26 +97,26 @@ namespace Topppro.SecureSite.Controllers
             return base.Edit(entity);
         }
 
-        [HttpPost]
-        public ActionResult Toggle(int id)
-        {
-            try
-            {
-                var entity =
-                    this.Business.Get(id);
+        //[HttpPost]
+        //public ActionResult Toggle(int id)
+        //{
+        //    try
+        //    {
+        //        var entity =
+        //            this.Business.Get(id);
 
-                entity.Enabled =
-                    !entity.Enabled;
+        //        entity.Enabled =
+        //            !entity.Enabled;
 
-                this.Business.Update(entity);
+        //        this.Business.Update(entity);
 
-                return Ok();
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(ex.Message);
-            }
-        }
+        //        return Ok();
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return BadRequest(ex.Message);
+        //    }
+        //}
 
         public override void CreateGetPrerender(Topppro.Entities.Distributor entity = null)
         {
---
+++ Topppro.SecureSite/Models/Menu.cs
@@ -0,0 +1,16 @@
+﻿namespace Topppro.SecureSite.Models
+{
+    public enum Menu_Enum
+    {
+        Dashboard,
+        Languages,
+        Categories,
+        Series,
+        Model,
+        Products,
+        Associations,
+        Countries,
+        Distributors,
+        Tools
+    }
+}
