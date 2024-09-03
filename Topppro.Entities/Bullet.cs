111d766a5199cea81115bb159f7c1533ec3b3c69
jmirancid <jmirancid@gmail.com>
2024-09-02 23:59:07 -0300

    added import export

--- Topppro.Entities/Bullet.cs
+++ Topppro.Entities/Bullet.cs
@@ -5,6 +5,7 @@ using Topppro.Entities.Resources;
 using xFNet.Common.Extensions;
 using xFNet.Entities;
 using xFNet.Entities.Resources;
+using xFNet.IO.Attributes;
 
 
 namespace Topppro.Entities
@@ -27,36 +28,36 @@ namespace Topppro.Entities
 
     public class Bullet_Metadata
     {
-        //[Import(Order = 0), Export(Order = 0)]
+        [Import(Order = 0), Export(Order = 0)]
         public int BulletId { get; set; }
 
         [Display(Name = "Entity_Product", ResourceType = typeof(Ent_BulletResource))]
         [Required(ErrorMessageResourceName = "Required", ErrorMessageResourceType = typeof(ValidationResource))]
         public int ProductId { get; set; }
 
-        //[Import(Order = 1), Export(Order = 1)]
+        [Import(Order = 1), Export(Order = 1)]
         [Display(Name = "Entity_Culture", ResourceType = typeof(Ent_BulletResource))]
         [Required(ErrorMessageResourceName = "Required", ErrorMessageResourceType = typeof(ValidationResource))]
         public int CultureId { get; set; }
 
         [AllowHtml]
         [DataType(DataType.MultilineText)]
-        //[Import(Order = 2), Export(Order = 2)]
+        [Import(Order = 2), Export(Order = 2)]
         [Display(Name = "Entity_Name", ResourceType = typeof(Ent_BulletResource))]
         [Required(ErrorMessageResourceName = "Required", ErrorMessageResourceType = typeof(ValidationResource))]
         public string Name { get; set; }
 
         [AllowHtml]
         [DataType(DataType.MultilineText)]
-        //[Import(Order = 3), Export(Order = 3)]
+        [Import(Order = 3), Export(Order = 3)]
         [Display(Name = "Entity_Value", ResourceType = typeof(Ent_BulletResource))]
         public string Value { get; set; }
 
-        //[Import(Order = 4), Export(Order = 4)]
+        [Import(Order = 4), Export(Order = 4)]
         [Display(Name = "Entity_Priority", ResourceType = typeof(Ent_BulletResource))]
         public Nullable<int> Priority { get; set; }
 
-        //[Import(Order = 5), Export(Order = 5)]
+        [Import(Order = 5), Export(Order = 5)]
         [Display(Name = "Entity_Enabled", ResourceType = typeof(Ent_BulletResource))]
         public bool Enabled { get; set; }
     }
---
+++ Topppro.SecureSite/Models/ImportViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web;
+using xFNet.Entities.Resources;
+
+namespace Topppro.SecureSite.Models
+{
+    public class ImportViewModel
+    {
+        public int EntityId { get; set; }
+
+        [Display(Name = "File")]
+        [Required(ErrorMessageResourceName = "Required", ErrorMessageResourceType = typeof(ValidationResource))]
+        public HttpPostedFileBase File { get; set; }
+    }
+}
